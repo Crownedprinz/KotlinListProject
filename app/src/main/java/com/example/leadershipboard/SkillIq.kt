a87b22d40b49a297296ecc2c13e8790726650f3f
Ademola John <AdemolaJhon@gmail.com>
2020-09-11 15:06:11 +0100

    First commit

---
+++ app/src/main/java/com/example/leadershipboard/SkillIq.kt
@@ -0,0 +1,11 @@
+package com.example.leadershipboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SkillIq: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
